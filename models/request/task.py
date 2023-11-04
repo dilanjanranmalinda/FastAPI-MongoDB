c4eba251a2f2affc83a3b373fce82a32e8a8506f
dilanjan <dilanjan@zmessenger.com>
2023-11-04 15:10:31 +0530

    set date/time in project

--- models/request/task.py
+++ models/request/task.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
+
 
 class Task(BaseModel):
     task_id: int
     title: str
     description: str
     status: bool
-    created_at: datetime
-    updated_at: datetime
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
